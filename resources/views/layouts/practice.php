5907943eaf0cd451ba96270c2c8a684aac6f6212
”onozawa-kazuya” <kazuya.jpee@gmail.com>
2020-05-06 05:16:33 +0000

    task13

--- resources/views/layouts/practice.php
+++ resources/views/layouts/practice.php
@@ -1,42 +1,23 @@
-<!--@extends('layouts.admin')-->
-<!--@section('title', 'ニュースの新規作成')-->
+<!--<!DOCTYPE html>-->
+<!--<html lang=" {{ app()->getLocale() }}">-->
+<!--    <meta charset="utf-8">-->
+<!--    <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+<!--    <meta name="viewport" content="width=device-width", "initial-scale=1">-->
+<!--    <meta name="csrf-token" content="{{ csrf_token() }}">-->
+    
+<!--    <title>@yield('title')</title>-->
+<!--</html>-->
 
-<!--@section('content')-->
-<!--    <div class="container">-->
-<!--        <div class="row">-->
-<!--            <div class="col-md-8 mx-auto">-->
-<!--                <h2>ニュース新規作成</h2>-->
-                <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+<!--@if (count($errors) > 0)-->
+<!--<ul>-->
+<!--    @foreach($errors->all() as $e)-->
+<!--    <li> {{ $e }} </li>-->
+<!--    @endforeach-->
+<!--</ul>-->
+<!--@endif-->
 
-                    @if (count($errors) > 0)
-                        <ul>
-                            @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                    <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">本文</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">画像</label>
-                        <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
-                        </div>
-                    </div>
-                    {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="更新">
-                </form>
-            </div>
-        </div>
-    </div>
-@endsection
+
+$text = sprintf("本日は%d年%d月%d日です", "2020", "4", "25");
+echo $text.PHP_EOL;
+
+echo "web: vendor/bin/heroku-php-apache2 public" > Procfile
